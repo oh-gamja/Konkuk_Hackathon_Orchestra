d892ea58f374d681d25061a1c1c029d5c29073fa
정소민 <jeongsomin@jeongsomin-ui-MacBookAir.local>
2023-08-05 11:01:34 +0900

    Chore: Project Setting

---
+++ backend/src/main/java/com/konkuk_hackathon/ohgamja/OhgamjaApplication.java
@@ -0,0 +1,14 @@
+package com.konkuk_hackathon.ohgamja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhgamjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhgamjaApplication.class, args);
+		System.out.println("ohgamja");
+	}
+
+}
---
+++ backend/src/test/java/com/konkuk_hackathon/ohgamja/OhgamjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk_hackathon.ohgamja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhgamjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
