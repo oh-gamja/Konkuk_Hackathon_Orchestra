54e7a9064d62cecef38ce002f1a4f981bcd429b7
nonaninona <starcraft0529@gmail.com>
2023-08-13 14:00:43 +0900

    카카오 로그인 준비

--- frontend/app/src/main/java/com/example/ohgamja_frontend/MainActivity.kt
+++ frontend/app/src/main/java/com/example/ohgamja_frontend/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.ohgamja_frontend
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.FragmentManager
 import com.example.ohgamja_frontend.databinding.ActivityMainBinding
+import com.example.ohgamja_frontend.ui.home.HomeFragment
+import com.example.ohgamja_frontend.ui.home.LikeFragment
+import com.example.ohgamja_frontend.ui.home.MypageFragment
+import com.example.ohgamja_frontend.ui.home.PlaylistsFragment
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +19,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_home -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, HomeFragment()).commit()
+                R.id.navigation_like -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, LikeFragment()).commit()
+                R.id.navigation_playlists -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, PlaylistsFragment()).commit()
+                else-> supportFragmentManager.beginTransaction().replace(R.id.main_frame, MypageFragment()).commit()
+            }
+            true
+        }
 
         setContentView(binding.root)
     }
