72efb70e83ad4ea8739f4e2b57f029e5706ace80
nonaninona <starcraft0529@gmail.com>
2023-08-13 13:47:43 +0900

    프로젝트 초기 세팅 완료

--- frontend/app/src/main/java/com/example/ohgamja_frontend/ui/like/LikeFragment.kt
+++ frontend/app/src/main/java/com/example/ohgamja_frontend/ui/like/LikeFragment.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.ohgamja_frontend.databinding.FragmentHomeBinding
+import com.example.ohgamja_frontend.databinding.FragmentLikeBinding
 
-class HomeFragment : Fragment() {
-    lateinit var binding: FragmentHomeBinding
+class LikeFragment : Fragment() {
+    lateinit var binding: FragmentLikeBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentLikeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
