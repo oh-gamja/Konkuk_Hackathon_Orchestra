695b0e3181443ceef5eeca0881eb02dbc4e69395
meo-young <meoyoung000@naver.com>
2023-08-14 23:12:36 +0900

    08.14.23:12 홈화면만 구성 기능x

--- frontend/app/src/main/java/com/example/ohgamja_frontend/ui/home/HomeFragment.kt
+++ frontend/app/src/main/java/com/example/ohgamja_frontend/ui/home/HomeFragment.kt
@@ -7,7 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ohgamja_frontend.MainActivity
 import com.example.ohgamja_frontend.databinding.FragmentHomeBinding
+import com.example.ohgamja_frontend.ui.RVAdapter
+import com.example.ohgamja_frontend.ui.RecyclerviewModel
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
@@ -17,6 +22,52 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        var items = mutableListOf<RecyclerviewModel>()
+
+        items.add(
+            RecyclerviewModel(
+                "젠가",
+                3,
+                "스릴러",
+                "4")
+        )
+        items.add(
+            RecyclerviewModel(
+                "경도",
+                2,
+                "범죄",
+                "2")
+        )
+        items.add(
+            RecyclerviewModel(
+                "바니바니",
+                2,
+                "농락",
+                "5")
+        )
+        items.add(
+            RecyclerviewModel(
+                "출석부",
+                1,
+                "속도",
+                "6")
+        )
+        items.add(
+            RecyclerviewModel(
+                "더 게임 오브 데스",
+                3,
+                "도박",
+                "8")
+        )
+        //1231232131
+        val rv = binding.rv
+        val rvAdapter = RVAdapter(this,requireContext(),items)
+        val mainActivity = activity as MainActivity
+
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(activity)
+
         return binding.root
     }
 
