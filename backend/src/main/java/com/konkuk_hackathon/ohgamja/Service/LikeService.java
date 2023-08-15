1b9a8a52da9e9f1534528cbad3afd053b55409cc
nonaninona <starcraft0529@gmail.com>
2023-08-15 10:43:35 +0900

    Like add, remove 추가

--- backend/src/main/java/com/konkuk_hackathon/ohgamja/Service/LikeService.java
+++ backend/src/main/java/com/konkuk_hackathon/ohgamja/Service/LikeService.java
@@ -1,7 +1,10 @@
 package com.konkuk_hackathon.ohgamja.Service;
 
 import com.konkuk_hackathon.ohgamja.Dao.LikeDao;
+import com.konkuk_hackathon.ohgamja.Domain.Game;
+import com.konkuk_hackathon.ohgamja.Domain.GameLike;
 import com.konkuk_hackathon.ohgamja.Domain.LikeTopGame;
+import com.konkuk_hackathon.ohgamja.Dto.Response.LikeResponse;
 import com.konkuk_hackathon.ohgamja.Dto.Response.LikeTopResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,5 +20,17 @@ public class LikeService {
     public LikeTopResponse getLikeTop() {
         List<LikeTopGame> likeTopGames =  likeDao.getLikeTop();
         return new LikeTopResponse(likeTopGames);
+    }
+
+    public LikeResponse addLike(Long memberId, Long gameId){
+        GameLike gameLike = new GameLike(memberId, gameId);
+        likeDao.save(gameLike);
+        return new LikeResponse("good");
+    }
+
+    public LikeResponse deleteLike(Long memberId, Long gameId){
+        GameLike gameLike = new GameLike(memberId, gameId);
+        likeDao.delete(gameLike);
+        return new LikeResponse("good");
     }
 }
