ab3a5419b0dc921be6f48ddf014d474df5ac5e7a
nonaninona <starcraft0529@gmail.com>
2023-08-16 19:57:42 +0900

    카카오톡 로그인 연동 완료

--- frontend/app/src/main/java/com/example/ohgamja_frontend/MainActivity.kt
+++ frontend/app/src/main/java/com/example/ohgamja_frontend/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.ohgamja_frontend
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ohgamja_frontend.ui.retrofit.RetrofitUtil
 import com.example.ohgamja_frontend.databinding.ActivityMainBinding
+import com.example.ohgamja_frontend.ui.KakaoLoginFragment
 import com.example.ohgamja_frontend.ui.home.HomeFragment
 import com.example.ohgamja_frontend.ui.home.MypageFragment
 import com.example.ohgamja_frontend.ui.like.LikeFragment
@@ -24,11 +26,35 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.navigation_home -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, HomeFragment()).commit()
-                R.id.navigation_like -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, LikeFragment()).commit()
-                R.id.navigation_random -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, LikeFragment()).commit()
+                R.id.navigation_like -> {
+                    if(RetrofitUtil.getAccessToken().isEmpty()){
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, KakaoLoginFragment()).commit()
+                    } else {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, LikeFragment()).commit()
+                    }
+                }
+                R.id.navigation_random -> {
+                    if(RetrofitUtil.getAccessToken().isEmpty()){
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, KakaoLoginFragment()).commit()
+                    } else {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, LikeFragment()).commit()
+                    }
+                }
                 //게임추천 api로 변경 예정
-                R.id.navigation_playlists -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, PlaylistsFragment()).commit()
-                else-> supportFragmentManager.beginTransaction().replace(R.id.main_frame, MypageFragment()).commit()
+                R.id.navigation_playlists -> {
+                    if(RetrofitUtil.getAccessToken().isEmpty()){
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, KakaoLoginFragment()).commit()
+                    } else {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, PlaylistsFragment()).commit()
+                    }
+                }
+                else-> {
+                    if(RetrofitUtil.getAccessToken().isEmpty()){
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, KakaoLoginFragment()).commit()
+                    } else {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_frame, MypageFragment()).commit()
+                    }
+                }
             }
             true
         }
--- frontend/app/src/main/java/com/example/ohgamja_frontend/ui/retrofit/RetrofitInterface.kt
+++ frontend/app/src/main/java/com/example/ohgamja_frontend/ui/retrofit/RetrofitInterface.kt
@@ -1,16 +1,14 @@
-package com.example.bangwool.retrofit
+package com.example.ohgamja_frontend.ui.retrofit
 
+import com.example.ohgamja_frontend.ui.retrofit.LoginRequest
+import com.example.ohgamja_frontend.ui.retrofit.LoginResponse
 import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
 import retrofit2.http.PUT
 
 interface RetrofitInterface {
+
 
 
 
@@ -18,6 +16,10 @@ interface RetrofitInterface {
 
 interface RetrofitLoginInterface {
 
+    @POST("/auth/kakao")
+    fun KaKaoLogin(
+        @Body requestBody: LoginRequest
+    ): Call<BaseResponse<LoginResponse>>
 
 
 }
