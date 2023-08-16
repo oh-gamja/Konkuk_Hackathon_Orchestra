0a2cb6d75d5c60215def095bdcb54c1cc0f77274
정소민 <yjk70702002@gmail.com>
2023-08-16 15:12:46 +0900

    feat: platlist 삭제

--- frontend/app/src/main/java/com/example/ohgamja_frontend/ui/playlists/PlaylistsFragment.kt
+++ frontend/app/src/main/java/com/example/ohgamja_frontend/ui/playlists/PlaylistsFragment.kt
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ohgamja_frontend.R
 import com.example.ohgamja_frontend.databinding.FragmentPlaylistsBinding
 import com.example.ohgamja_frontend.ui.RVAdapter_playList
 
@@ -29,18 +32,18 @@ class PlaylistsFragment : Fragment() {
 
         val items = arrayListOf<PlaylistViewModel>()
 
-        items.add(PlaylistViewModel("플레이리스트1",3))
-        items.add(PlaylistViewModel("플레이리스트2",4))
-        items.add(PlaylistViewModel("플레이리스트3",5))
-        items.add(PlaylistViewModel("플레이리스트4",6))
-        items.add(PlaylistViewModel("플레이리스트5",7))
-        items.add(PlaylistViewModel("플레이리스트6",8))
-        items.add(PlaylistViewModel("플레이리스트7",9))
-        items.add(PlaylistViewModel("플레이리스트8",10))
-        items.add(PlaylistViewModel("플레이리스트10",11))
+        items.add(PlaylistViewModel(false, "플레이리스트1",3))
+        items.add(PlaylistViewModel(false, "플레이리스트2",4))
+        items.add(PlaylistViewModel(false, "플레이리스트3",5))
+        items.add(PlaylistViewModel(false, "플레이리스트4",6))
+        items.add(PlaylistViewModel(false, "플레이리스트5",7))
+        items.add(PlaylistViewModel(false, "플레이리스트6",8))
+        items.add(PlaylistViewModel(false, "플레이리스트7",9))
+        items.add(PlaylistViewModel(false, "플레이리스트8",10))
+        items.add(PlaylistViewModel(false, "플레이리스트10",11))
 
         val rv = binding.playRv
-        val playlistsAdapter = PlaylistsAdapter(items)
+        val playlistsAdapter = PlaylistsAdapter(requireContext(), requireActivity().supportFragmentManager, items)
 
         rv.adapter = playlistsAdapter
         rv.layoutManager = LinearLayoutManager(context)
@@ -48,6 +51,16 @@ class PlaylistsFragment : Fragment() {
         binding.playlistAddButton.setOnClickListener {
             val dialog = NewListDialogFragment()
             dialog.show(requireActivity().supportFragmentManager,"DialogFragment")
+        }
+
+        binding.deleteListIv.setOnClickListener {
+            playlistsAdapter.stateChange()
+            binding.playRv.adapter?.notifyDataSetChanged()
+        }
+
+        binding.deleteListTv.setOnClickListener {
+            playlistsAdapter.stateChange()
+            binding.playRv.adapter?.notifyDataSetChanged()
         }
 
 
