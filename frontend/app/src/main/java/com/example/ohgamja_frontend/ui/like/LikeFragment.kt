95d065541484423240517fb351a53d15237acd72
meo-young <meoyoung000@naver.com>
2023-08-16 16:06:40 +0900

    recyclerview 아이템 배치 중 device 오류 발생

--- frontend/app/src/main/java/com/example/ohgamja_frontend/ui/like/LikeFragment.kt
+++ frontend/app/src/main/java/com/example/ohgamja_frontend/ui/like/LikeFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ohgamja_frontend.databinding.FragmentLikeBinding
+import com.example.ohgamja_frontend.ui.RVAdapter
+import com.example.ohgamja_frontend.ui.RecyclerviewModel
 
 class LikeFragment : Fragment() {
     lateinit var binding: FragmentLikeBinding
@@ -15,11 +18,56 @@ class LikeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLikeBinding.inflate(inflater, container, false)
-        //카카오 로그인 버튼 누를시 동작 구현
+
+        var items = mutableListOf<RecyclerviewModel>()
+
+        /*//카카오 로그인 버튼 누를시 동작 구현
         val login_btn = binding.loginBtn
         login_btn.setOnClickListener {
 
-        }
+        }*/
+
+        items.add(
+            RecyclerviewModel(
+                "젠가",
+                3,
+                "스릴러",
+                "4")
+        )
+        items.add(
+            RecyclerviewModel(
+                "젠가",
+                3,
+                "스릴러",
+                "4")
+        )
+        items.add(
+            RecyclerviewModel(
+                "젠가",
+                3,
+                "스릴러",
+                "4")
+        )
+        items.add(
+            RecyclerviewModel(
+                "젠가",
+                3,
+                "스릴러",
+                "4")
+        )
+        items.add(
+            RecyclerviewModel(
+                "젠가",
+                3,
+                "스릴러",
+                "4")
+        )
+
+        val rv = binding.likerv
+        val rvAdapter = RVAdapter(this,requireContext(),items)
+
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(context)
 
         return binding.root
     }
